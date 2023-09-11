980a96c51845bc64684b3a513fc6282600317d7f
ThiccHedgehog <michallonski23042017@gmail.com>
2023-09-11 10:15:56 +0200

    Zmienne

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,59 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //System.out.printf("Hello and welcome!");
+        System.out.print("Nasza lekcja");
 
+        System.out.println("Hej!");
+
+        //liczbowe
+
+        //int i = 3;
+        //Integer intiger = null;
+
+        //Long l = 9L;
+        //long l2 = 9L;
+
+       // Double d = 2.0;
+        //double d2 = 2.0;
+
+        //true/false
+
+       // Boolean bol = true;
+        //boolean bol2 = false;
+
+        //text
+
+       // String s = "Hello world";
+       // String e = "Witom";
+       // String w = "Test";
+       // String t = "wewee";
+        //System.out.println(s);
+        //System.out.println(e);
+        //System.out.println(w);
+        //System.out.println(t);
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+
+        int liczba1 = 1;
+        int liczba2 = 2;
+        int liczba3 = liczba1 + liczba2;
+        System.out.println(liczba1 + "+" + liczba2 + "=" + liczba3);
+
+
+        double l1 = 1.6;
+        double l2 = 2.2;
+        double l3 = l1 - l2;
+        System.out.println(l1 + "-" + l2 + "=" + l3);
+
+        int p1 = 2;
+        int p2 = 4;
+        double p3 = Math.pow(p1, p2);
+        System.out.println(p1 + "^" + p2 + "=" + p3);
+
+
         }
     }
-}
+
